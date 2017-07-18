7247ef27a9186d9fdb582d858f4f9c876be88b3d
Michel Ribeiro <michelrbr@gmail.com>
2017-07-18 17:39:01 -0300

    Initial commit

---
+++ app/src/main/java/CueDotApplication.java
@@ -0,0 +1,28 @@
+import android.app.Application;
+import android.os.StrictMode;
+
+import br.com.mxel.cuedot.BuildConfig;
+
+/**
+ * Created by michelribeiro on 18/07/17.
+ */
+
+public class CueDotApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if(BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+        }
+    }
+}
