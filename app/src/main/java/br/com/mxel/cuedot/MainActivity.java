aefe8409c81c5adb29b8c6046c00202d944a8fab
Michel Ribeiro <michelrbr@gmail.com>
2017-07-19 16:30:12 -0300

    Add some static code analysis tools

--- app/src/main/java/br/com/mxel/cuedot/MainActivity.java
+++ app/src/main/java/br/com/mxel/cuedot/MainActivity.java
@@ -10,4 +10,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // SF_SWITCH_NO_DEFAULT
+    // SF_SWITCH_FALLTHROUGH
+    /*private void findBugsFailTest(int var) {
+        switch (var) {
+            case 1:
+                System.out.println("1");
+            case 2:
+                System.out.println("2");
+        }
+    }*/
+
+    /*private void pmdFailTest(int a, int b, int c, int d) {
+        if (a > b) {
+            if (b > c) {
+                if (c > d) {
+                    if (d > a)
+                        System.out.println("PMD");
+                        // some logic
+
+                }
+            }
+        }
+    }*/
 }
