c2038392dad15fdf1a2cd113abb79a7154874fa4
Michel Ribeiro <michelrbr@gmail.com>
2018-02-21 21:55:04 -0300

    Layout improvements

--- app/src/main/java/br/com/mxel/cuedot/movies/adapter/MoviesAdapter.java
+++ app/src/main/java/br/com/mxel/cuedot/movies/adapter/MoviesAdapter.java
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import br.com.mxel.cuedot.R;
 import br.com.mxel.cuedot.data.model.Movie;
 import br.com.mxel.cuedot.movieDetail.MovieDetailActivity;
 import timber.log.Timber;
@@ -29,7 +31,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MovieViewHolder> {
 
     @Override
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.activity_list_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_movie, parent, false);
         MovieViewHolder holder = new MovieViewHolder(view);
         holder.itemView.setOnClickListener(v -> {
 
@@ -49,11 +51,13 @@ public class MoviesAdapter extends RecyclerView.Adapter<MovieViewHolder> {
 
         Movie movie = _data.get(position);
         holder.itemView.setTag(position);
-        ((TextView) holder.itemView.findViewById(android.R.id.text1)).setText(movie.getTitle());
+        ((TextView) holder.itemView.findViewById(R.id.movie_title)).setText(movie.getTitle());
+        ((TextView) holder.itemView.findViewById(R.id.release_date_text_view)).setText(movie.getReleaseDate());
+        ((RatingBar) holder.itemView.findViewById(R.id.rating)).setRating(movie.getRating() / 2);
         Picasso.with(holder.itemView.getContext())
                 .load(movie.getPosterPath())
                 //.centerCrop()
-                .into((ImageView) holder.itemView.findViewById(android.R.id.icon));
+                .into((ImageView) holder.itemView.findViewById(R.id.movie_cover));
     }
 
     @Override
